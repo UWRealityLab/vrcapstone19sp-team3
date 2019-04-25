60b7fb24a1d1fec1f47fa5b9e09e287d6bffdc18
keumjo <keumjo@tangleroad.com>
2019-04-25 16:09:13 -0700

    working with demo audio clip

--- apis/APITests/APITests/Main.cs
+++ apis/APITests/APITests/Main.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Cloud.Speech.V1;
+using Google.Apis.Auth.OAuth2;
+using System.IO;
+using Grpc.Auth;
 
 namespace APITests
 {
@@ -10,7 +14,31 @@ namespace APITests
     {
         static public void Main()
         {
-            Console.WriteLine("Main Method");
+            SpeechSettings settings = new SpeechSettings();
+
+            GoogleCredential googleCredential;
+            using (Stream m = new FileStream("./arcap.json", FileMode.Open))
+                googleCredential = GoogleCredential.FromStream(m);
+            var channel = new Grpc.Core.Channel(SpeechClient.DefaultEndpoint.Host,
+                googleCredential.ToChannelCredentials());
+            var speech = SpeechClient.Create(channel);
+            var config = new RecognitionConfig
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Flac,
+                SampleRateHertz = 16000,
+                LanguageCode = LanguageCodes.English.UnitedStates
+            };
+            var audio = RecognitionAudio.FromStorageUri("gs://cloud-samples-tests/speech/brooklyn.flac");
+
+            var response = speech.Recognize(config, audio);
+
+            foreach (var result in response.Results)
+            {
+                foreach (var alternative in result.Alternatives)
+                {
+                    Console.WriteLine(alternative.Transcript);
+                }
+            }
         }
     }
 }
