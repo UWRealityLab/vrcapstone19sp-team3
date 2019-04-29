88f99b1639a7775f80fa0256324d5dfed1361c20
James Kim <jamesgki.kim67@gmail.com>
2019-04-28 17:22:06 -0700

    Populated list of languages and some UI elements

---
+++ chatassist/ChatAssist/Assets/Scripts/LanguageScrollList.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class LanguageScrollList : MonoBehaviour
+{
+    private List<System.String> languageList = new List<System.String>();
+    public Transform contentPanel;
+    public GameObject prefab;
+    public ToggleGroup toggleGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        populateList();
+        AddButtons();
+    }
+
+    private void populateList()
+    {
+        StreamReader stream = new StreamReader("./languageList.txt");
+        while (!stream.EndOfStream)
+        {
+            languageList.Add(stream.ReadLine());
+        }
+        stream.Close();
+        Debug.Log(languageList.Count);
+    }
+    
+    private void AddButtons()
+    {
+        Debug.Log("Adding buttons");
+        for (int i = 0; i < languageList.Count; i++)
+        {
+            // Make object
+            GameObject newBtn = (GameObject)GameObject.Instantiate(prefab, contentPanel);
+            newBtn.SetActive(true);
+            Debug.Log(languageList[i]);
+            Toggle tog = newBtn.GetComponent<Toggle>();
+            tog.name = languageList[i] + "Toggle";
+            tog.GetComponentInChildren<Text>().text = languageList[i];
+            tog.group = toggleGroup;
+            //Debug.Log(t);
+            //
+            //newBtn.GetComponentInChildren<Text>().text = languageList[i];
+            //newBtn.GetComponentInChildren<Text>().text = languageList[i];
+        }
+        Debug.Log("Added buttons");
+    }
+
+    
+}
---
+++ chatassist/ChatAssist/Assets/Scripts/LanguageToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageToggle : MonoBehaviour
+{
+    public Toggle toggle;
+    public Text languageName;
+    
+    private LanguageScrollList scrollList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //button.on.AddListener(HandleClick);
+    }
+
+    public void SetUp(string l, LanguageScrollList currentScrollList)
+    {
+        languageName.text = l;
+        this.scrollList = currentScrollList;
+    }
+
+    public void HandleClick()
+    {
+
+    }
+}
