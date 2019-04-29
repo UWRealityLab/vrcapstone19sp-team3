e84991e31bfa75ff5b4f9bdef323ddf73704ffca
James Kim <jamesgki.kim67@gmail.com>
2019-04-28 17:35:55 -0700

    Added basic prefabs with Speech bubble and text generation outline, still needs to be fleshed out

---
+++ chatassist/ChatAssist/Assets/Scripts/PanelGen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelGen : MonoBehaviour
+{
+    public GameObject speechToTextPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(original: speechToTextPanel, new Vector3(100, 100, 100), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ chatassist/ChatAssist/Assets/Scripts/SpeechToText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechToText : MonoBehaviour
+{
+    public Text text;
+    public int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Hello World";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count < 10)
+        {
+            count++;
+            text.text = count.ToString();
+        }
+        else
+        {
+            text.text = "Goodbye World";
+        }
+    }
+}
