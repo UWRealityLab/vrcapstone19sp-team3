fff2f9a87355d52f1636776c1ab5aad201f4026f
James Kim <jamesgki.kim67@gmail.com>
2019-05-01 20:51:10 -0700

    Changed speech to text generation scene to automatically generate bubbles at hard coded locations, progress through hard-coded text, and have them face the camera of the user

--- chatassist/ChatAssist/Assets/Scripts/PanelGen.cs
+++ chatassist/ChatAssist/Assets/Scripts/PanelGen.cs
@@ -4,16 +4,60 @@ using UnityEngine;
 
 public class PanelGen : MonoBehaviour
 {
+    // Generates the Speech bubbles for the user
+    // TODO: anchor speechToTextPanel to visual indicator i.e. marker or Magic Leap
+    // For now, we currently only generate 4 panels with same text. Will need to be adjusted
+    // for multiple users pending on how Google API works for multiple outputs
+
     public GameObject speechToTextPanel;
+    public Camera m_Camera;
+    //public Transform transform;
+    private GameObject[] speechToTextInstantiations;
+    public int count;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(original: speechToTextPanel, new Vector3(100, 100, 100), Quaternion.identity);
+        //Debug.Log("Started");
+        speechToTextInstantiations = new GameObject[4];
+        speechToTextInstantiations[0] = Instantiate(original: speechToTextPanel, new Vector3(100, 0, 0), Quaternion.identity);
+        speechToTextInstantiations[1] = Instantiate(original: speechToTextPanel, new Vector3(-100, 0, 0), Quaternion.identity);
+        speechToTextInstantiations[2] = Instantiate(original: speechToTextPanel, new Vector3(0, 0, 100), Quaternion.identity);
+        speechToTextInstantiations[3] = Instantiate(original: speechToTextPanel, new Vector3(0, 0, -100), Quaternion.identity);
+
+        /*
+        for (int i = 0; i < 4; i++)
+        {
+            speechToTextInstantiations[i] = Instantiate(original: speechToTextPanel, new Vector3(100 * (i % 2 - 1), 0, 100 * ((i + 1) % 2 - 1)), Quaternion.identity);
+        }
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (count == 10000)
+        {
+            //speechToTextInstantiation.SetActive(false);
+            Destroy(speechToTextInstantiations[0]);
+        } else
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                speechToTextInstantiations[i].transform.LookAt(m_Camera.transform);
+            }
+            count++;
+        }
+    }
+
+    void LateUpdate()
+    {
+        /*
+        for (int i = 0; i < 4; i++)
+        {
+              speechToTextInstantiations[i].transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back,
+                    m_Camera.transform.rotation * Vector3.up);
+        }
+        */
     }
 }
--- chatassist/ChatAssist/Assets/Scripts/SpeechToText.cs
+++ chatassist/ChatAssist/Assets/Scripts/SpeechToText.cs
@@ -2,29 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SpeechToText : MonoBehaviour
 {
-    public Text text;
+    // Script to grab text from Google API and display text to user via TextMeshPro component
+    // Note: Following is just an example, still need to hook up Google API via getSpeechToText call on Update
+    // Can check with parent if we are looking at the target, and start grabbing text from there via text.tranform.parent
+    public TextMeshProUGUI text;
     public int count = 0;
+    private string currString;
 
     // Start is called before the first frame update
     void Start()
     {
-        text.text = "Hello World";
+        currString = "Lorem ipsum dolor sit amet consectetur adipiscing elit nisi, platea at neque porta nostra scelerisque a eleifend ornare, cum accumsan cursus torquent parturient laoreet condimentum. Suspendisse congue faucibus cum sodales sem euismod, mollis dis augue primis dui nullam, erat conubia massa posuere fames. Justo varius erat netus id libero auctor malesuada cum dapibus, hac dis conubia enim ligula bibendum tincidunt nullam, vulputate integer eu non commodo vitae congue lobortis." +
+            "Metus euismod sodales nibh nascetur duis parturient eu commodo, sed mauris per urna quis laoreet suspendisse, fames neque magnis mus a leo suscipit.Interdum nisi natoque porttitor nulla ridiculus molestie ut, arcu pretium ligula augue mollis ac velit, parturient faucibus hac porta et tristique.Pulvinar dui in gravida dis sociis mollis semper ornare, pellentesque turpis a eu quis himenaeos ut inceptos, felis senectus lobortis taciti arcu habitant nulla.Nullam fringilla etiam tempus aliquam pellentesque sociosqu iaculis cubilia arcu, posuere cras praesent ligula sem parturient lacinia lectus penatibus sodales, duis facilisis porttitor varius diam egestas conubia velit.";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (count < 10)
+        getSpeechToText();
+    }
+
+    void getSpeechToText()
+    {
+        if ((count / 10) * 80 + 80 < currString.Length)
         {
+            //count++;
+            text.text = currString.Substring((count / 10) * 80, 80);
             count++;
-            text.text = count.ToString();
-        }
-        else
-        {
-            text.text = "Goodbye World";
         }
     }
 }
