28047c8bca586972a2be8e07eb7fcc0f2bc58393
keumjo <keumjo@tangleroad.com>
2019-05-02 19:05:25 -0700

    add speech to text

--- chatassist/ChatAssist/Assets/Scripts/SpeechToText.cs
+++ chatassist/ChatAssist/Assets/Scripts/SpeechToText.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using WebSocketSharp;
+
 
 public class SpeechToText : MonoBehaviour
 {
@@ -12,18 +14,42 @@ public class SpeechToText : MonoBehaviour
     public TextMeshProUGUI text;
     public int count = 0;
     private string currString;
+    private WebSocket ws;
 
     // Start is called before the first frame update
     void Start()
     {
         currString = "Lorem ipsum dolor sit amet consectetur adipiscing elit nisi, platea at neque porta nostra scelerisque a eleifend ornare, cum accumsan cursus torquent parturient laoreet condimentum. Suspendisse congue faucibus cum sodales sem euismod, mollis dis augue primis dui nullam, erat conubia massa posuere fames. Justo varius erat netus id libero auctor malesuada cum dapibus, hac dis conubia enim ligula bibendum tincidunt nullam, vulputate integer eu non commodo vitae congue lobortis." +
             "Metus euismod sodales nibh nascetur duis parturient eu commodo, sed mauris per urna quis laoreet suspendisse, fames neque magnis mus a leo suscipit.Interdum nisi natoque porttitor nulla ridiculus molestie ut, arcu pretium ligula augue mollis ac velit, parturient faucibus hac porta et tristique.Pulvinar dui in gravida dis sociis mollis semper ornare, pellentesque turpis a eu quis himenaeos ut inceptos, felis senectus lobortis taciti arcu habitant nulla.Nullam fringilla etiam tempus aliquam pellentesque sociosqu iaculis cubilia arcu, posuere cras praesent ligula sem parturient lacinia lectus penatibus sodales, duis facilisis porttitor varius diam egestas conubia velit.";
+        Debug.Log("connecting to ws...");
+        ws = new WebSocket("ws://127.0.0.1:8887/");
+        ws.OnMessage += response_get;
+        ws.Connect();
+        ws.Send("./peter.flac");
     }
 
     // Update is called once per frame
     void Update()
     {
-        getSpeechToText();
+        if (this.text.text != this.currString)
+        {
+            this.text.text = currString;
+        }
+    }
+
+    private void response_get(object sender, MessageEventArgs mssg)
+    {
+        string data = mssg.Data;
+        Debug.Log("speech server says2: " + data);
+        Debug.Log("step1: " + data);
+        //this.mydude.text = data;
+        Debug.Log("step2: " + data);
+        this.currString = data;
+        Debug.Log("step3: " + data);
+        Debug.Log("set currtext to " + data);
+        //this.mydude.text = "helloabc!!!";
+        //this.mydude.text = data;
+        //this.UpdateText(data);
     }
 
     void getSpeechToText()
